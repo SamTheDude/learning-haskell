e5110a822d1b5c749302c0413138f5b5fb1e80b5
Sam_The_Dude <kent_sam@outlook.com>
2020-09-28 15:14:45 +0100

    Comments do be wacky.

--- obligatory-hello-world/hello-world.hs
+++ obligatory-hello-world/hello-world.hs
@@ -1,1 +1,7 @@
+{- 
+Imagine using a multiline comment for a single line.
+-}
+
+-- Then using a single line comment and using that to put all the other stuff in just because this repository is the biggest meme I've ever made.
+
 main = putStrLn "Hello World!"
